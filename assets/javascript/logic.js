c858ba4f86f52ebc5c5ecc42abccbeceb81964a8
dennis-nguyen <nguyen.denn@gmail.com>
2017-04-27 00:49:52 -0700

    Added Local Storage

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,25 +1,42 @@
 var topics = ["sushi", "bbq", "icecream"];
-var colors = ["red", "blue", "yellow", "orange", "teal", "black", "green", "lightblue", "darkgrey", "pink"];
+var colors = [
+    "red",
+    "blue",
+    "yellow",
+    "orange",
+    "teal",
+    "black",
+    "green",
+    "lightblue",
+    "darkgrey",
+    "pink"
+];
 
 $(document).ready(function () {
-
     function makeButtons() {
+        $(".buttonArea").empty();
+        if (localStorage.getItem("topics")) {
+            topics = JSON.parse(localStorage.getItem("topics"));
+        }
         for (i = 0; i < topics.length; i++) {
-            $(".buttonArea").append("<button class='topics' data-topic=" + topics[i] + ">" + topics[i]);
+            $(".buttonArea").append("<button class='topics' data-topic=" + topics[i].split(" ").join("+") + ">" + topics[i]);
         }
     }
     //Adds Buttons, Didn't want to push and call makeButtons every time.
     function addButton() {
         if ($("#sushiSearch").val().length > 0) {
-            var enteredText = $("#sushiSearch").val().split(" ").join("+");
-            $(".buttonArea").append("<button class='topics' data-topic=" + enteredText + ">" + enteredText.split("+").join(" "));
+            var enteredText = $("#sushiSearch").val();
+            topics.push(enteredText);
+            topics = JSON.stringify(topics);
+            localStorage.setItem("topics", topics);
         }
+        makeButtons();
     }
 
     function appendGifs(response) {
         $(".gifArea").empty();
-        var randomNumber = (Math.floor(Math.random() * 10) + 1);
-        for (i = randomNumber; i < randomNumber+10; i++) {
+        var randomNumber = Math.floor(Math.random() * 10) + 1; //Choosing Random Index for Gifs
+        for (i = randomNumber; i < randomNumber + 10; i++) {
             var gifDiv = $("<div class='col-lg-6 text-center'> </div>");
             var rating = $("<h2>").append("Rating: " + response.data[i].rating);
             var gifImage = $("<img>").attr("src", response.data[i].images.fixed_height_downsampled.url);
@@ -41,7 +58,7 @@ $(document).ready(function () {
                 rating: "pg",
                 limit: 25,
                 api_key: "dc6zaTOxFJmzC"
-            },
+            }
         }).done(function (response) {
             appendGifs(response);
         });
@@ -59,14 +76,27 @@ $(document).ready(function () {
         changeColor();
     }
 
+    function clearStorage() {
+        topics.splice(topics.length - 1, 1); //Splicing out the last item of the topics array
+        topics = JSON.stringify(topics); //
+        localStorage.setItem("topics", topics);
+        makeButtons();
+    }
+
     function changeColor() {
-        $(".topTitle").css("color", colors[Math.floor(Math.random() * colors.length)]);
+        $(".topTitle").css(
+            "color",
+            colors[Math.floor(Math.random() * colors.length)]
+        );
     }
 
     function applyClickHandlers() {
         $(".submit").on("click", function () {
             addButton();
-            $('#sushiSearch').val("");
+            $("#sushiSearch").val("");
+        });
+        $(".refund").on("click", function () {
+            clearStorage();
         });
     }
     applyClickHandlers();
